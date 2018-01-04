dac6aea07d5024fc1ee7c303c72f5465ec75c26a
INABA Akira <inaba@ulula.local>
2018-01-04 23:43:53 +0900

    [Chap.02] Python's "for" Loop Understands Lists

---
+++ chap02/marvin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- mode: python; coding: utf-8 -*-
+# file: marvin.py
+#    Created:       <2018/01/04 23:36:53>
+#    Last Modified: <2018/01/04 23:38:01>
+
+paranoid_android = "Marvin"
+letters = list(paranoid_android)
+for char in letters:
+    print('\t', char)
