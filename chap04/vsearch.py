2264b80826337832b91e2ebd9eea111db1eed127
INABA Akira <inaba.akira@gmail.com>
2018-02-11 20:00:35 +0900

    [Chap.04] Functions Can Accept Arguments

--- chap04/vsearch.py
+++ chap04/vsearch.py
@@ -2,12 +2,11 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/11 17:26:00>
+#    Last Modified: <2018/02/11 19:39:54>
 
-def search4vowels():
-    """Display any vowels found in an asked-for word."""
+def search4vowels(word):
+    """Display any vowels found in a supplied word."""
     vowels = set('aeiou')
-    word = input('Provide a word to search for vowels: ')
     found = vowels.intersection(set(word))
     for vowel in found:
         print(vowel)
