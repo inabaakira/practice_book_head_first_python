d644c712e60cf64f106c529b4246881ea1a28f90
INABA Akira <inaba.akira@gmail.com>
2018-02-12 16:18:54 +0900

    [Chap.04] Use Annotations to Improve Your Docs

--- chap04/vsearch.py
+++ chap04/vsearch.py
@@ -2,9 +2,9 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/12 11:56:21>
+#    Last Modified: <2018/02/12 16:15:27>
 
-def search4vowels(word):
+def search4vowels(word:str) -> set:
     """Display any vowels found in a supplied word."""
     vowels = set('aeiou')
     return vowels.intersection(set(word))
