c92df1e627a052d63767841e3f70068ebc371dbb
INABA Akira <inaba.akira@gmail.com>
2018-02-12 11:49:21 +0900

    [Chap.04] Returning One Value

--- chap04/vsearch.py
+++ chap04/vsearch.py
@@ -2,11 +2,10 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/11 19:39:54>
+#    Last Modified: <2018/02/12 11:41:24>
 
 def search4vowels(word):
     """Display any vowels found in a supplied word."""
     vowels = set('aeiou')
     found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    return bool(found)
