e168f4aedb5849a4166343201786189766866827
INABA Akira <inaba.akira@gmail.com>
2018-02-12 13:28:25 +0900

    [Chap.04] Returning More Than One Value

--- chap04/vsearch.py
+++ chap04/vsearch.py
@@ -2,10 +2,9 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/12 11:41:24>
+#    Last Modified: <2018/02/12 11:56:21>
 
 def search4vowels(word):
     """Display any vowels found in a supplied word."""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    return bool(found)
+    return vowels.intersection(set(word))
