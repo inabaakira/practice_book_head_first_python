f38e13f577ebf06e102f145ff680f8fbc4827432
INABA Akira <inaba.akira@gmail.com>
2018-02-14 20:36:08 +0900

    [Chap.04] Creating Another Function, 1 of 3

--- chap04/vsearch.py
+++ chap04/vsearch.py
@@ -2,9 +2,12 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/14 20:20:47>
+#    Last Modified: <2018/02/14 20:35:34>
 
 def search4vowels(phrase:str) -> set:
     """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(word))
+
+
+def search4letters(phrase:str, letters:str) -> set:
