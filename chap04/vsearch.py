2b8b4f18bb1ed714c6e132fbce85e203b5e1cb39
INABA Akira <inaba.akira@gmail.com>
2018-02-14 20:26:17 +0900

    [Chap04] Making a Generically Useful Function

--- chap04/vsearch.py
+++ chap04/vsearch.py
@@ -2,9 +2,9 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/12 19:45:57>
+#    Last Modified: <2018/02/14 20:20:47>
 
-def search4vowels(word:str) -> set:
-    """Return any vowels found in a supplied word."""
+def search4vowels(phrase:str) -> set:
+    """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(word))
