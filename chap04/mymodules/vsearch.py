2e30afcfbc62cc8201fd71f689c5f131ad94747f
INABA Akira <inaba.akira@gmail.com>
2018-02-24 19:23:58 +0900

    [Chap.04] Understanding the Failure Messages

--- chap04/mymodules/vsearch.py
+++ chap04/mymodules/vsearch.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-#-*- mode: python; coding: utf-8 -*-
+# -*- mode: python; coding: utf-8 -*-
 # file: vsearch.py
 #    Created:       <2018/02/08 20:36:09>
-#    Last Modified: <2018/02/14 21:17:08>
+#    Last Modified: <2018/02/22 20:55:08>
 
-def search4vowels(phrase:str) -> set:
+
+def search4vowels(phrase: str) -> set:
     """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
 
-def search4letters(phrase:str, letters:str='aeiou') -> set:
+def search4letters(phrase: str, letters: str='aeiou') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
