b9a2418af47227beb3fccdcc790b3db7fec6d4db
INABA Akira <inaba.akira@gmail.com>
2018-03-06 23:07:32 +0900

    [Chap.05] Exposing Functionality to the Web

--- chap05/webapp/hello_flask.py
+++ chap05/webapp/hello_flask.py
@@ -2,14 +2,19 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: hello_flask.py
 #    Created:       <2018/02/26 20:27:55>
-#    Last Modified: <2018/02/26 20:35:52>
+#    Last Modified: <2018/03/06 23:13:48>
 
 from flask import Flask
+from vsearch import search4letters
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
     return 'Hello world from Flask!'
+
+@app.route('/search4')
+def do_search() -> str:
+    return str(search4letters('life, the universe, and everything', 'eiru,!'))
 
 app.run()
