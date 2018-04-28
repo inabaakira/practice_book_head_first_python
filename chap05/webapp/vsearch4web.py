bc483a2726d266f992b5c46ff1d6b34f8abe9290
INABA Akira <inaba.akira@gmail.com>
2018-04-28 21:00:13 +0900

    [Chap.05] Calculating the Data We Need

--- chap05/webapp/vsearch4web.py
+++ chap05/webapp/vsearch4web.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: hello_flask.py
 #    Created:       <2018/02/26 20:27:55>
-#    Last Modified: <2018/04/09 13:57:04>
+#    Last Modified: <2018/04/28 20:31:12>
 
 from flask import Flask, render_template, request
 from vsearch import search4letters
@@ -14,14 +14,21 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results)
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
 
-app.run(debug=True)
+app.run(host='0.0.0.0', debug=True)
