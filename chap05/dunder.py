21f0cf00c9355c41f93c0e43f3d16f211b920616
INABA Akira <inaba.akira@gmail.com>
2018-05-05 19:21:08 +0900

    [Chap.05] Preparing Your Webapp for the Cloud

---
+++ chap05/dunder.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- mode: python; coding: utf-8 -*-
+# file: dunder.py
+#    Created:       <2018/05/05 19:10:20>
+#    Last Modified: <2018/05/05 19:19:36>
+
+print('We start off in:', __name__)
+if __name__ == '__main__':
+    print('And end up in:', __name__)
