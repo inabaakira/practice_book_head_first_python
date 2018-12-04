b1370e53058c88532399478c812e25f9c8c242b2
INABA Akira <inaba.akira@gmail.com>
2018-12-05 01:24:44 +0900

    [Chap.06] A Better Open, Process, Close: "with": log_request を追加

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: hello_flask.py
 #    Created:       <2018/02/26 20:27:55>
-#    Last Modified: <2018/05/05 20:20:57>
+#    Last Modified: <2018/12/05 01:21:26>
 
 from flask import Flask, render_template, request
 from vsearch import search4letters
@@ -15,7 +15,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
-
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
@@ -27,6 +27,9 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
