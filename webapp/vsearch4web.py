55b59ea2258f7608cd9ef812490d35cb94bf371a
INABA akira <inaba.akira@gmail.com>
2018-12-13 12:45:39 +0000

    [Chap.06] Consider What You're Trying to Do: 終了

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: hello_flask.py
 #    Created:       <2018/02/26 20:27:55>
-#    Last Modified: <2018/12/13 20:49:20>
+#    Last Modified: <2018/12/13 21:33:09>
 
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
@@ -27,9 +27,8 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
-# def log_request(req: 'flask_request', res: str) -> None:
-#     with open('vsearch.log', 'a') as log:
-#         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+import mysql.connector
 
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the results."""
@@ -37,9 +36,6 @@ def log_request(req: 'flask_request', res: str) -> None:
                  'user': 'vsearch',
                  'password': 'vsearchpasswd',
                  'database': 'vsearchlogDB', }
-
-    import mysql.connector
-
     conn = mysql.connector.connect(**dbconfig)
     cursor = conn.cursor()
     _SQL = """insert into log
