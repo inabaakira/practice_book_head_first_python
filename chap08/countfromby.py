842ff12a08ceebe2715752b26a1ac6edb29f7ab5
INABA akira <inaba.akira@gmail.com>
2018-12-19 11:46:02 +0000

    [Chap.08] Defining CountFromBy's Representation: 終了

--- chap08/countfromby.py
+++ chap08/countfromby.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: countfromby.py
 #    Created:       <2018/12/18 20:46:15>
-#    Last Modified: <2018/12/19 00:20:20>
+#    Last Modified: <2018/12/19 20:42:39>
 
 class CountFromBy:
     def __init__(self, v: int, i: int) -> None:
@@ -10,3 +10,5 @@ class CountFromBy:
         self.incr = i
     def increase(self) -> None:
         self.val += self.incr
+    def __repr__(self) -> str:
+        return str(self.val)
