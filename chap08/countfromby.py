6557e384fdae7328b3b5ed546e9e5dce5c97a61a
INABA akira <inaba.akira@gmail.com>
2018-12-19 11:54:23 +0000

    [Chap.08] Providing Sensible Defaults for CountFromBy: 終了

--- chap08/countfromby.py
+++ chap08/countfromby.py
@@ -2,13 +2,16 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: countfromby.py
 #    Created:       <2018/12/18 20:46:15>
-#    Last Modified: <2018/12/19 20:42:39>
+#    Last Modified: <2018/12/19 20:51:34>
 
 class CountFromBy:
-    def __init__(self, v: int, i: int) -> None:
+
+    def __init__(self, v: int=0, i: int=1) -> None:
         self.val = v
         self.incr = i
+
     def increase(self) -> None:
         self.val += self.incr
+
     def __repr__(self) -> str:
         return str(self.val)
