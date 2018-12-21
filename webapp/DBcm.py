8f2dfd33fc6d54a193fa7aa3f6dc1432f1762eb9
INABA akira <inaba.akira@gmail.com>
2018-12-21 09:04:20 +0000

    [Chap.09] Perform Teardown with Dunder "exit": 終了

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -7,8 +7,11 @@ class UseDatabase:
 
     def __enter__(self) -> 'cursor':
         self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor
+        self.cursor = self.conn.cursor()
         return self.cursor
 
-    def __exit__(self):
-        pass
+    def __exit__(self, exc_type, exc_value, exc_trace) -> None:
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
