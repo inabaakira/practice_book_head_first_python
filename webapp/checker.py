3c3586be942caa63c946b9fd9b4d9b3c5c4633a8
INABA Akira <inaba.akira@gmail.com>
2019-01-02 06:30:03 +0000

    [Chap.10] Your Decorator in All Its Glory: 終了

--- webapp/checker.py
+++ webapp/checker.py
@@ -2,11 +2,14 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: checker.py
 #    Created:       <2018/12/31 21:43:09>
-#    Last Modified: <2019/01/02 12:38:04>
+#    Last Modified: <2019/01/02 15:29:06>
 
 from flask import session
 
+from functools import wraps
+
 def check_logged_in(func):
+    @wraps(func)
     def wrapper(*args, **kwargs) -> str:
         if 'logged_in' in session:
             return func(*args, **kwargs)
