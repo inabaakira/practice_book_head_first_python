1224db70fe7f2062d10f25c4ff95424498ed413e
INABA Akira <inaba.akira@gmail.com>
2019-01-02 06:40:43 +0000

    [Chap.10] Putting Your Decorator to Work: 終了

--- webapp/simple_webapp.py
+++ webapp/simple_webapp.py
@@ -2,9 +2,11 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: simple_webapp.py
 #    Created:       <2018/12/26 20:25:41>
-#    Last Modified: <2018/12/28 20:13:44>
+#    Last Modified: <2019/01/02 15:23:51>
 
 from flask import Flask, session
+
+from checker import check_logged_in
 
 app = Flask(__name__)
 
@@ -13,14 +15,17 @@ def hello() -> str:
     return 'Hello from the simple webapp.'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is page 1.'
 
 @app.route('/page2')
+@check_logged_in
 def page2() -> str:
     return 'This is page 2.'
 
 @app.route('/page3')
+@check_logged_in
 def page3() -> str:
     return 'This is page 3.'
 
@@ -33,12 +38,6 @@ def do_login() -> str:
 def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out.'
-
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged in.'
-    return 'You are NOT logged in.'
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
 
