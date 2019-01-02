22aef052b42cc7ce207fe887b593d5910e0e297a
INABA Akira <inaba.akira@gmail.com>
2019-01-02 14:37:29 +0900

    [Chap.10] The Final Step: Handling Arguments: 終了

--- webapp/checker.py
+++ webapp/checker.py
@@ -2,11 +2,13 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: checker.py
 #    Created:       <2018/12/31 21:43:09>
-#    Last Modified: <2019/01/01 15:03:57>
+#    Last Modified: <2019/01/02 12:38:04>
+
+from flask import session
 
 def check_logged_in(func):
-    def wrapper() -> str:
+    def wrapper(*args, **kwargs) -> str:
         if 'logged_in' in session:
-            return func()
+            return func(*args, **kwargs)
         return 'You are NOT logged in.'
     return wrapper
