07709ae30deda21b5a6f699107c9af0153d60cb2
INABA akira <inaba.akira@gmail.com>
2019-02-13 12:02:56 +0000

    [Chap.13] That Feeling You Get...: 終了

--- chap13/do_convert.py
+++ chap13/do_convert.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: convert.py
 #    Created:       <2019/02/06 21:07:00>
-#    Last Modified: <2019/02/12 20:29:51>
+#    Last Modified: <2019/02/12 21:37:59>
 
 from datetime import datetime
 import pprint
@@ -22,7 +22,10 @@ print()
 
 fts = {convert2ampm(k): v.title() for k, v in flights.items()}
 
-when = {}
-for dest in set(fts.values()):
-    when[dest] = [k for k, v in fts.items() if v == dest]
+pprint.pprint(fts)
+print()
+
+when = {dest: [k for k, v in fts.items() if v == dest] for dest in set(fts.values())}
+
 pprint.pprint(when)
+print()
