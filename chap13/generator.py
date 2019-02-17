7002486dbfbef4e0ca951dd6fba8931bc18d1662
INABA Akira <inaba.akira@gmail.com>
2019-02-17 15:41:33 +0900

    [Chap.13] Tracing Your Generator Function, 2 of 2: 終了

--- chap13/generator.py
+++ chap13/generator.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: generator.py
 #    Created:       <2019/02/14 21:37:25>
-#    Last Modified: <2019/02/17 15:31:00>
+#    Last Modified: <2019/02/17 15:40:21>
 
 from url_utils import gen_from_urls
 
@@ -10,3 +10,9 @@ urls = ('http://headfirstlabs.com', 'http://oreilly.com', 'http://twitter.com')
 
 for resp_len, status, url in gen_from_urls(urls):
     print(resp_len, '->', status, '->', url)
+
+urls_res = {url: size for size, _, url in gen_from_urls(urls)}
+
+import pprint
+
+pprint.pprint(urls_res)
